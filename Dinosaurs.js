ce893d9dae6dcffc2e2d5f6ba0b82e8e15c7a228
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 13:10:15 -0400

    Update Dinosaurs.js

--- Dinosaurs.js
+++ Dinosaurs.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Dinosaurs = () => {
+  return (
+    <div>
+      <h1>Dinosaurs Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default Dinosaurs;
