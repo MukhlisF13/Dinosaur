16e16c1ca396050321fb58388835254192f585bd
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 13:54:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
