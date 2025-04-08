b6e47f4af38a4384e7949ca1ebfd2f2229c1daf5
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 15:27:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
-import App from './app'; 
+import Home from './app'; 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
