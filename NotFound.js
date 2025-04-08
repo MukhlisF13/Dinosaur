b203d5914976c4b43096000cf1a87e0339f9e5e5
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 13:14:50 -0400

    Create NotFound.js

---
+++ NotFound.js
@@ -0,0 +1,18 @@
+import React from `react`;
+import { link } from `react-router-dom`;
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: `center`, padding: `50px 20px`}}>
+  <h1>404 - Page Not Found</h1>
+<p>Oops! Looks Like this page has gone extinct.</p>
+<div style={{ margin: `30px 0` }}>
+<Link to="/" className="nav-link">
+  Return to Homepage
+  </Link>
+  </div>
+  </div>
+);
+};
+
+export default NotFound;
