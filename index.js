c35684d298f9804d2679da553d6d69c0ef55f32e
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 15:35:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './style.css';
-import Home from './app'; 
+import { Link } from 'react-router-dom';
+import Home from './Home';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
+const Index = () => {
+  return <Home />;
+};
 
-    <App />
-
-);
+export default Index;
