0228ec11288ef1e03c4eaee90c923713f89e3b41
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 05:32:09 -0400

    Create Navbar.js

---
+++ Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="nav-container">
+        <NavLink to="/" className={({ isActive }) => 
+          isActive ? 'nav-link active' : 'nav-link'
+        }>
+          Home
+        </NavLink>
+        <NavLink to="/dinosaurs" className={({ isActive }) => 
+          isActive ? 'nav-link active' : 'nav-link'
+        }>
+          All Dinosaurs
+        </NavLink>
+        <NavLink to="/media" className={({ isActive }) => 
+          isActive ? 'nav-link active' : 'nav-link'
+        }>
+          Dinosaur Media
+        </NavLink>
+        <NavLink to="/merch" className={({ isActive }) => 
+          isActive ? 'nav-link active' : 'nav-link'
+        }>
+          Merch
+        </NavLink>
+        <NavLink to="/contact" className={({ isActive }) => 
+          isActive ? 'nav-link active' : 'nav-link'
+        }>
+          Contact Us
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
