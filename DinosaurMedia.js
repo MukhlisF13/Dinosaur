f98ab6020e403cdd225b1155b591dac7a9738416
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 13:10:23 -0400

    Update DinosaurMedia.js

--- DinosaurMedia.js
+++ DinosaurMedia.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const DinosaurMedia = () => {
+  return (
+    <div>
+      <h1>Dinosaur Media Page</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default DinosaurMedia;
