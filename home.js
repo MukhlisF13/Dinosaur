f9fe262dd45b8f78ff655d4f2c10ab1a41ab1e38
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-08 13:52:35 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import ReactDOM  from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
   const [showFunFact, setShowFunFact] = useState(false);
