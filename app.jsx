e4ce3cd419911b04d7b5ab7731891e6536a49f18
MukhlisF13 <MukhlisF@nycstudents.net>
2025-04-10 13:32:50 -0400

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Dinosaurs from './pages/Dinosaurs'; 
